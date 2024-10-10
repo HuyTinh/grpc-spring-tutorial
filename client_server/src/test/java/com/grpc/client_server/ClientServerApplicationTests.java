4c0471ded452129d673f30be29decc12967c0e88
Huy Tinh <170064219+HuyTinh@users.noreply.github.com>
2024-10-10 20:49:37 +0700

    first commit.

---
+++ client_server/src/test/java/com/grpc/client_server/ClientServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grpc.client_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
